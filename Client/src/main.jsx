52386628f0105c543cfd85ce6a5c793eda6d14d1
rogulik <malvla2008@gmail.com>
2023-08-24 14:37:20 +0300

    React project initialized

---
+++ Client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import './reset.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+const router = createBrowserRouter([])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>,
+)
